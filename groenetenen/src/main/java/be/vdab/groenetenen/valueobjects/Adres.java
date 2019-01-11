cf5fc5032b1aa2c1302b2a68f1d949b1fb84fdf5
Bianca Reymen <Bianca.Reymen@pandora.be>
2019-01-11 13:16:18 +0100

    hoofdstuk 2

---
+++ groenetenen/src/main/java/be/vdab/groenetenen/valueobjects/Adres.java
@@ -0,0 +1,52 @@
+package be.vdab.groenetenen.valueobjects;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotBlank;
+
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.SafeHtml;
+
+@Embeddable
+public class Adres implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@NotBlank @SafeHtml
+	private String straat;
+	@NotBlank @SafeHtml
+	private String huisNr;
+	@NotBlank @Range(min = 1000, max = 9999)
+	private int postcode;
+	@NotBlank @SafeHtml
+	private String gemeente;
+	
+	
+	public Adres() {
+		super();
+	}
+	
+	public Adres(@NotBlank @SafeHtml String straat, @NotBlank @SafeHtml String huisNr,
+			@NotBlank @Range(min = 1000, max = 9999) int postcode, @NotBlank @SafeHtml String gemeente) {
+		super();
+		this.straat = straat;
+		this.huisNr = huisNr;
+		this.postcode = postcode;
+		this.gemeente = gemeente;
+	}
+	
+	public String getStraat() {
+		return straat;
+	}
+	public String getHuisNr() {
+		return huisNr;
+	}
+	public int getPostcode() {
+		return postcode;
+	}
+	public String getGemeente() {
+		return gemeente;
+	}
+
+	
+}
