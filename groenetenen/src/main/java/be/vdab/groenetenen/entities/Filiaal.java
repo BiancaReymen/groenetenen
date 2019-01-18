859a480ae421807ebf4c2f7bb74c1c4749f4c450
Bianca Reymen <Bianca.Reymen@pandora.be>
2019-01-18 15:40:56 +0100

    hoofdstuk 10

--- groenetenen/src/main/java/be/vdab/groenetenen/entities/Filiaal.java
+++ groenetenen/src/main/java/be/vdab/groenetenen/entities/Filiaal.java
@@ -18,6 +18,7 @@ import javax.persistence.Version;
 import javax.validation.Valid;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -31,9 +32,8 @@ import org.springframework.format.annotation.NumberFormat;
 import org.springframework.format.annotation.NumberFormat.Style;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
-import com.sun.istack.NotNull;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import be.vdab.groenetenen.adapters.LocalDateAdapter;
 import be.vdab.groenetenen.valueobjects.Adres;
